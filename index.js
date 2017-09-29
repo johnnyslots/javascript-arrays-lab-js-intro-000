6b8db4e56ca91f63605cac9064e7b83ecb07fa34
Yoni Slotwiner <yonislots@gmail.com>
2017-09-29 21:58:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const app = "I don't do much."
 
 function destructivelyAppendKitten(kittens,name){
-  var kittens = ["Milo", "Otis", "Garfield"];
-  var name = "Ralph";
   kittens.push(name);
   return kittens;
 };
